78c52ec488ecf9575d4bf26e94cab5cb7166c819
Jeremy <jeremy.inoa@atdev.do>
2024-07-07 01:51:34 -0400

    feat: extended functionality

--- FormatConverter.js
+++ FormatConverter.js
@@ -17,3 +17,65 @@ export const ToWebP = async (imageFile) => {
         img.src = URL.createObjectURL(imageFile);
     });
 };
+
+export const ToPNG = async (imageFile) => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => {
+            const canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+            canvas.toBlob((blob) => {
+                resolve(blob);
+            }, 'image/png', 1);
+        };
+        img.onerror = (error) => {
+            reject(error);
+        };
+        img.src = URL.createObjectURL(imageFile);
+    });
+};
+
+export const ToJPG = async (imageFile) => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => {
+            const canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+            canvas.toBlob((blob) => {
+                resolve(blob);
+            }, 'image/jpeg', 1);
+        };
+        img.onerror = (error) => {
+            reject(error);
+        };
+        img.src = URL.createObjectURL(imageFile);
+    });
+};
+
+export const ToPDF = async (imageFile) => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => {
+            const canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+            
+            canvas.toBlob((blob) => {
+                const pdfBlob = new Blob([blob], { type: 'application/pdf' });
+                resolve(pdfBlob);
+            }, 'image/jpeg', 1);
+        };
+        img.onerror = (error) => {
+            reject(error);
+        };
+        img.src = URL.createObjectURL(imageFile);
+    });
+};
